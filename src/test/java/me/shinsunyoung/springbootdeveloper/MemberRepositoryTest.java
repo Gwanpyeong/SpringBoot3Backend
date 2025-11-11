27b8cceaf7cca626f223745e33fdc11001fd4f46
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-11 13:28:41 +0900

    MemberRepositoryTest

---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,24 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        assertThat(members.size()).isEqualTo(3);
+    }
+}
