b7ecc634cc80b54c64895f332ed83df3501b690e
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-11 15:57:28 +0900

    7. 06장 블로그 API

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,15 @@
+package me.shinsunyoung.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+
+}
---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.shinsunyoung.springbootdeveloper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
